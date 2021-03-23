64d2e311bfe26a182aa7e7f6f9e6f31bebe79082
Corey Balon <cmb136@arl.psu.edu>
2021-03-23 10:49:40 -0400

    fix individual designer links

---
+++ design/2021-03-23-links-fix.py
@@ -0,0 +1,33 @@
+from repo.models import Profile, ExperOrg, DesignTeam
+from exper.models import Organization, Study, Experiment, SessionTeam, Exercise, CustomLinks
+from exper.models import Structure, Role, Position, Group, GroupPosition
+from chat.models import Channel, ChannelPosition
+
+roleProcess = Role.objects.filter(name="Process").first()
+roleBusiness = Role.objects.filter(name="Business").first()
+roleOps = Role.objects.filter(name="OpsPlanner").first()
+roleDesign = Role.objects.filter(name="Designer").first()
+
+strProcess = Structure.objects.filter(name="Process Manager").first()
+strProcessAI = Structure.objects.filter(name="Process Manager (AI)").first()
+strSwitch = Structure.objects.filter(name="Switch").first()
+strNoSwitch = Structure.objects.filter(name="No Switch").first()
+strExtra = Structure.objects.filter(name="Extra").first()
+
+links_to_fix = CustomLinks.objects.filter(link="/static/docs/process/team_problem_statement_designspecialist.pdf")
+for link_to_fix in links_to_fix:
+    link_to_fix.is_team = True
+    link_to_fix.save()
+
+organizations = Organization.objects.all()
+for organization in organizations:
+    if organization.name == "ARL" or organization.name == "PSU" or organization.name == "PSU 2" or organization.name == "CMU" or organization.name == "CMU 2":
+        pm_study = Study.objects.filter(name="2021 Spring Process Manager", organization=organization).first()
+        if pm_study:
+            CustomLinks.objects.create(
+                text="Individual Design Brief",
+                link="/static/docs/individual_study.pdf",
+                link_type=3,
+                is_team=False,
+                study=pm_study,
+                )
